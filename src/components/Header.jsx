5937685e275f6a7b8155bf72967978f6fd5ab9c6
mohanrajmurus <mohanrajmurus@gmail.com>
2023-02-20 21:49:52 +0530

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import cart from "../assets/icons/cart.svg";
+import hamburger from "../assets/icons/hamburger.png";
+import close from "../assets/icons/close.png";
+import search from '../assets/icons/search.png'
+import {Link} from 'react-router-dom'
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(true);
+  return (
+    <div className="container">
+      <header className="header">
+        <div className={isOpen?"header--menubar":"header--menubar "} onClick={()=>setIsOpen(!isOpen)}>
+          {isOpen ? (
+            <img src={hamburger} alt="menubar" />
+          ) : (
+            <img src={close} alt="menubar" />
+          )}
+        </div>
+        <div className={isOpen?"header--logo": 'header--logo--mobile'}>
+         <Link to={'/'}>
+         <h1 className="header--logo__text">Store</h1>
+         </Link>
+        </div>
+        <div className="header--searchbar">
+          <input type="text" name="search-products" placeholder="search for a products" />
+          <img src={search} alt="search-icon" className="search--icon"/>
+        </div>
+        <div className={isOpen?"header--profile":"header--profile--mobile"}>
+          <button className="login">Login</button>
+        </div>
+        <div className="header--cart">
+          <img src={cart} alt="" className="cart--icon" />
+          <span className="cart--count">20</span>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from '../components/Header'
+const Home = () => {
+  return (
+    <div className='home--container'>
+      <Header/>
+    </div>
+  )
+}
+
+export default Home
